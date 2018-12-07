44d7734a700d81a391f691c9ea2a9a4afee413da
jooname <joona.eskonpekka@metropolia.fi>
2018-12-07 13:50:09 +0200

    buttonit

--- app/src/main/java/theryhma/sovellus/views/start/MenuFragment.java
+++ app/src/main/java/theryhma/sovellus/views/start/MenuFragment.java
@@ -31,25 +31,33 @@ public class MenuFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_menu, container, false);
 
+        view.findViewById(R.id.tip).setOnClickListener(buttonListener);
+        view.findViewById(R.id.til).setOnClickListener(buttonListener);
+        view.findViewById(R.id.kys).setOnClickListener(buttonListener);
+        view.findViewById(R.id.kal).setOnClickListener(buttonListener);
+
         return view;
     }
 
-    public void onButtonClick(View view) {
-        int id = view.getId();
-        switch (id) {
-            case R.id.kal:
+    private final View.OnClickListener buttonListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.kal:
 
-                break;
-            case R.id.til:
-                break;
-            case R.id.kys:
-                //Intent nextActivity = new Intent(getActivity(), DetailsActivity.class);
-                //nextActivity.putExtra("presidentIndex", i);
-                //startActivity(nextActivity);
-                break;
-            case R.id.tip:
-                break;
+                    break;
+                case R.id.til:
+                    break;
+                case R.id.kys:
+                    //Intent nextActivity = new Intent(getActivity(), DetailsActivity.class);
+                    //nextActivity.putExtra("presidentIndex", i);
+                    //startActivity(nextActivity);
+                    break;
+                case R.id.tip:
+                    //Intent intent = new Intent(getActivity(), TestiAktiviteetti.class);
+                    //startActivity(intent);
+                    break;
+            }
         }
-    }
+    };
 
 }
