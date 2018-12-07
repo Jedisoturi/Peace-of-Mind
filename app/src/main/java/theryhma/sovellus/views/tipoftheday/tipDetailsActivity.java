b101f4b33a3de97194ddacbb3313f72f34fb7e68
Tiia Toivonen <tiia.toivonen@metropolia.fi>
2018-12-07 12:15:58 +0200

    listviewtips

---
+++ app/src/main/java/theryhma/sovellus/views/tipoftheday/tipDetailsActivity.java
@@ -0,0 +1,13 @@
+package theryhma.sovellus.views.tipoftheday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tipDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_details);
+    }
+}
