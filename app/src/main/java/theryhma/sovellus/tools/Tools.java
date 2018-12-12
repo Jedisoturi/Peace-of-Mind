d3ea3266b90eb1c8a98198173560bb668d7876ce
JappaTheHutt <jonathan.paasonen@metropolia.fi>
2018-12-12 13:36:54 +0200

    osa dokumentointia

--- app/src/main/java/theryhma/sovellus/tools/Tools.java
+++ app/src/main/java/theryhma/sovellus/tools/Tools.java
@@ -2,7 +2,7 @@ package theryhma.sovellus.tools;
 
 import java.util.Calendar;
 
-/** This code compares two Calendar objects' dates, and checks whether they are same the same or not.
+/** This code compares two Calendar objects' dates, and checks whether they are the same or not.
  * This method is used in the Tip Of The Day -generator.*/
 public class Tools {
     public static boolean isSameDay(Calendar date1, Calendar date2) {
